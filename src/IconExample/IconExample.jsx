fbfa85d6a4604f8c5b82d59270dedb92a46cfa87
Afsal K <makafsal@gmail.com>
2024-10-16 18:14:16 +0530

    carbon icon import from @carbon/icons-react

---
+++ src/IconExample/IconExample.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Add } from "@carbon/react/icons";
+
+function IconExample() {
+  return (
+    <div className="datagrid-example-container">
+      <Add size="24" />
+    </div>
+  );
+}
+
+export default IconExample;
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Example from './Example/Example';
 import { ThemeProvider } from './ThemeSelector/ThemeContext';
 import { ThemeDropdown } from './ThemeSelector/ThemeDropdown';
 import { UIShellHeader } from './Example/Header';
+import IconExample from './IconExample/IconExample';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -16,7 +17,7 @@ root.render(
   <React.StrictMode>
     <ThemeProvider>
       <UIShellHeader />
-      <Example />
+      <IconExample />
       <ThemeDropdown />
     </ThemeProvider>
   </React.StrictMode>
