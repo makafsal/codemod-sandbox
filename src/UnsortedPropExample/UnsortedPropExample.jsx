1fdaf427ab2e064c600df77a7d48e5f0bcb429de
Afsal K <makafsal@gmail.com>
2024-10-18 11:46:03 +0530

    unsorted props example

---
+++ src/UnsortedPropExample/UnsortedPropExample.jsx
@@ -0,0 +1,8 @@
+import { Button } from "@carbon/react";
+import React from "react";
+
+function UnsortedPropExample() {
+  return <Button kind="danger" className="nothing">Button</Button>
+}
+
+export default UnsortedPropExample;
