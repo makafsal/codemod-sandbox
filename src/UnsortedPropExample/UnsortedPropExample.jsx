3f93651c477f0fd1fdd2c4cf3f0f5ff4795a715b
Afsal K <makafsal@gmail.com>
2024-10-18 11:59:22 +0530

    dummy component with unsorted proptypes

--- src/UnsortedPropExample/UnsortedPropExample.jsx
+++ src/UnsortedPropExample/UnsortedPropExample.jsx
@@ -2,7 +2,26 @@ import { Button } from "@carbon/react";
 import React from "react";
 
 function UnsortedPropExample() {
-  return <Button kind="danger" className="nothing">Button</Button>
+  return <h2>Component with unsorted propTypes</h2>;
 }
 
 export default UnsortedPropExample;
+
+UnsortedPropExample.propTypes = {
+  description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  disabled: PropTypes.bool,
+  actionsPlacement: PropTypes.oneOf(["top", "bottom"]),
+  children: PropTypes.node,
+  className: PropTypes.string,
+  clickZone: PropTypes.oneOf(["one", "two", "three"]),
+  actionIcons: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      icon: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+      onKeyDown: PropTypes.func,
+      onClick: PropTypes.func,
+      iconDescription: PropTypes.string,
+      href: PropTypes.string
+    })
+  )
+};
